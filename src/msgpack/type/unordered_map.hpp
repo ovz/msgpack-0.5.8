e2ddd8a1f743ae6181bd17d04533c72f5a47df43
ovz <ovz@yahoo.com>
2014-03-22 12:20:02 -0700

    Decomposed MSGPACK_UNORDERED_MAP macro to support template for standard hashers and facilitate customization with user-defined ones.

--- src/msgpack/type/unordered_map.hpp
+++ src/msgpack/type/unordered_map.hpp
@@ -23,11 +23,11 @@
 
 namespace msgpack {
 
-/// Had to define a macro to suppport unordered_map with custom hashers. 
+/// Had to rework unordered map support code into macros to support custom hashers.  
 /// Not all C++ compiler allow default template arguments for functions
-#define MSGPACK_UNORDERED_MAP_WITH_STD_HASH(K, V, hasher_type) \
-template <typename K, typename V> \
-inline std::unordered_map<K, V, std::hash<hasher_type>> operator>> (object o, std::unordered_map<K, V, std::hash<hasher_type>>& v) \
+
+#define MSGPACK_UNORDERED_MAP_CONCRETE_OPERATOR_UNPACK(K, V, H) \
+inline std::unordered_map<K, V, H> operator>> (object o, std::unordered_map<K, V, H>& v) \
 { \
 	if(o.type != type::MAP) { throw type_error(); } \
 	object_kv* p(o.via.map.ptr); \
@@ -38,93 +38,108 @@ inline std::unordered_map<K, V, std::hash<hasher_type>> operator>> (object o, st
 		p->val.convert(&v[key]); \
 	} \
 	return v; \
-} \
- \
-template <typename Stream, typename K, typename V> \
-inline packer<Stream>& operator<< (packer<Stream>& o, const std::unordered_map<K,V,std::hash<hasher_type>>& v) \
-{ \
-	o.pack_map(v.size()); \
-	for(typename std::unordered_map<K,V,std::hash<hasher_type>>::const_iterator it(v.begin()), it_end(v.end()); \
-			it != it_end; ++it) { \
-		o.pack(it->first); \
-		o.pack(it->second); \
-	} \
-	return o; \
-} \
- \
-template <typename K, typename V> \
-inline void operator<< (object::with_zone& o, const std::unordered_map<K,V,std::hash<hasher_type>>& v) \
-{ \
-	o.type = type::MAP; \
-	if(v.empty()) { \
-		o.via.map.ptr  = NULL; \
-		o.via.map.size = 0; \
-	} else { \
-		object_kv* p = (object_kv*)o.zone->malloc(sizeof(object_kv)*v.size()); \
-		object_kv* const pend = p + v.size(); \
-		o.via.map.ptr  = p; \
-		o.via.map.size = v.size(); \
-		typename std::unordered_map<K,V,std::hash<hasher_type>>::const_iterator it(v.begin()); \
-		do { \
-			p->key = object(it->first, o.zone); \
-			p->val = object(it->second, o.zone); \
-			++p; \
-			++it; \
-		} while(p < pend); \
-	} \
-} \
- \
- \
-template <typename K, typename V> \
-inline std::unordered_multimap<K, V, std::hash<hasher_type>> operator>> (object o, std::unordered_multimap<K, V, std::hash<hasher_type>>& v) \
-{ \
-	if(o.type != type::MAP) { throw type_error(); } \
-	object_kv* p(o.via.map.ptr); \
-	object_kv* const pend(o.via.map.ptr + o.via.map.size); \
-	for(; p != pend; ++p) { \
-		std::pair<K, V> value; \
-		p->key.convert(&value.first); \
-		p->val.convert(&value.second); \
-		v.insert(value); \
-	} \
-	return v; \
-} \
- \
-template <typename Stream, typename K, typename V> \
-inline packer<Stream>& operator<< (packer<Stream>& o, const std::unordered_multimap<K,V>& v) \
-{ \
-	o.pack_map(v.size()); \
-	for(typename std::unordered_multimap<K,V>::const_iterator it(v.begin()), it_end(v.end()); \
-			it != it_end; ++it) { \
-		o.pack(it->first); \
-		o.pack(it->second); \
-	} \
-	return o; \
-} \
- \
-template <typename K, typename V> \
-inline void operator<< (object::with_zone& o, const std::unordered_multimap<K,V>& v) \
-{ \
-	o.type = type::MAP; \
-	if(v.empty()) { \
-		o.via.map.ptr  = NULL; \
-		o.via.map.size = 0; \
-	} else { \
-		object_kv* p = (object_kv*)o.zone->malloc(sizeof(object_kv)*v.size()); \
-		object_kv* const pend = p + v.size(); \
-		o.via.map.ptr  = p; \
-		o.via.map.size = v.size(); \
-		typename std::unordered_multimap<K,V>::const_iterator it(v.begin()); \
-		do { \
-			p->key = object(it->first, o.zone); \
-			p->val = object(it->second, o.zone); \
-			++p; \
-			++it; \
-		} while(p < pend); \
-	} \
 }
 
-MSGPACK_UNORDERED_MAP_WITH_STD_HASH(K, V, K)
+template <typename K, typename V> 
+MSGPACK_UNORDERED_MAP_CONCRETE_OPERATOR_UNPACK(K, V, std::hash<K>)
+
+#define MSGPACK_UNORDERED_MAP_CONCRETE_OPERATOR_PACK(K, V, H) \
+inline packer<Stream>& operator<< (packer<Stream>& o, const std::unordered_map<K, V, H>& v) \
+{ \
+	o.pack_map(v.size()); \
+	for(typename std::unordered_map<K, V, H>::const_iterator it(v.begin()), it_end(v.end()); \
+			it != it_end; ++it) { \
+		o.pack(it->first); \
+		o.pack(it->second); \
+	} \
+	return o; \
+} 
+
+template <typename Stream, typename K, typename V> 
+MSGPACK_UNORDERED_MAP_CONCRETE_OPERATOR_PACK(K, V, std::hash<K>)
+
+#define MSGPACK_UNORDERED_MAP_CONCRETE_OPERATOR_PACK_OBJECT(K, V, H) \
+inline void operator<< (object::with_zone& o, const std::unordered_map<K, V, H>& v) \
+{ \
+	o.type = type::MAP; \
+	if(v.empty()) { \
+		o.via.map.ptr  = NULL; \
+		o.via.map.size = 0; \
+	} else { \
+		object_kv* p = (object_kv*)o.zone->malloc(sizeof(object_kv)*v.size()); \
+		object_kv* const pend = p + v.size(); \
+		o.via.map.ptr  = p; \
+		o.via.map.size = v.size(); \
+		typename std::unordered_map<K, V, H>::const_iterator it(v.begin()); \
+		do { \
+			p->key = object(it->first, o.zone); \
+			p->val = object(it->second, o.zone); \
+			++p; \
+			++it; \
+		} while(p < pend); \
+	} \
+} 
+
+template <typename K, typename V> 
+MSGPACK_UNORDERED_MAP_CONCRETE_OPERATOR_PACK_OBJECT(K, V, std::hash<K>)
+
+#define MSGPACK_UNORDERED_MAP_WITH_HASH(K, V, H) \
+MSGPACK_UNORDERED_MAP_CONCRETE_OPERATOR_UNPACK(K, V, H) \
+template <typename Stream> \
+MSGPACK_UNORDERED_MAP_CONCRETE_OPERATOR_PACK(K, V, H) \
+MSGPACK_UNORDERED_MAP_CONCRETE_OPERATOR_PACK_OBJECT(K, V, H) 
+
+
+// Original implementation of multimap
+
+template <typename K, typename V>
+inline std::unordered_multimap<K, V> operator>> (object o, std::unordered_multimap<K, V>& v)
+{
+	if(o.type != type::MAP) { throw type_error(); }
+	object_kv* p(o.via.map.ptr);
+	object_kv* const pend(o.via.map.ptr + o.via.map.size);
+	for(; p != pend; ++p) {
+		std::pair<K, V> value;
+		p->key.convert(&value.first);
+		p->val.convert(&value.second);
+		v.insert(value);
+	}
+	return v;
+}
+
+template <typename Stream, typename K, typename V>
+inline packer<Stream>& operator<< (packer<Stream>& o, const std::unordered_multimap<K,V>& v)
+{
+	o.pack_map(v.size());
+	for(typename std::unordered_multimap<K,V>::const_iterator it(v.begin()), it_end(v.end());
+			it != it_end; ++it) {
+		o.pack(it->first);
+		o.pack(it->second);
+	}
+	return o;
+}
+
+template <typename K, typename V>
+inline void operator<< (object::with_zone& o, const std::unordered_multimap<K,V>& v)
+{
+	o.type = type::MAP;
+	if(v.empty()) {
+		o.via.map.ptr  = NULL;
+		o.via.map.size = 0;
+	} else {
+		object_kv* p = (object_kv*)o.zone->malloc(sizeof(object_kv)*v.size());
+		object_kv* const pend = p + v.size();
+		o.via.map.ptr  = p;
+		o.via.map.size = v.size();
+		typename std::unordered_multimap<K,V>::const_iterator it(v.begin());
+		do {
+			p->key = object(it->first, o.zone);
+			p->val = object(it->second, o.zone);
+			++p;
+			++it;
+		} while(p < pend);
+	}
+}
 
 }  // namespace msgpack
 
